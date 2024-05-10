bbc1bdbd65a8c4201616d69cf9748b9bb0dfa92b
Anthony <anthonylsa12@gmail.com>
2024-05-09 21:42:56 -0700

    Add place works with limitations to founded years

--- models/places.js
+++ models/places.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose')
 
 const placeSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  pic: String,
+  pic: { type: String, default: 'https://www.wrestlezone.com/wp-content/uploads/sites/8/2023/12/kurt-angle-meme-machine.jpg'},
   cuisines: { type: String, required: true },
   city: { type: String, default: 'Anytown' },
   state: { type: String, default: 'USA' },
-  founded: Number
+  founded: {
+    type: Number,
+    min: [1673],
+    max: [new Date().getFullYear()]
+  }
 })
 
 placeSchema.methods.showEstablished = function() {
