d2ad619d573dd58d27220a93d2552325fe459e19
Anthony <anthonylsa12@gmail.com>
2024-05-09 21:29:11 -0700

    updated database with starter restaurants

--- seeders/seed-places.js
+++ seeders/seed-places.js
@@ -1,17 +1,25 @@
-const db = require('./models')
+const db = require('../models')
 
 db.Place.create([{
     name: 'H-Thai-ML',
     city: 'Seattle',
     state: 'WA',
     cuisines: 'Thai, Pan-Asian',
-    pic: '/images/h-thai-ml-tables.jpg',
+    pic: '/images/padThai.jpg',
     founded: 1989
 }, {
     name: 'Coding Cat Cafe',
     city: 'Phoenix',
     state: 'AZ',
     cuisines: 'Coffee, Bakery',
-    pic: '/images/coffee-cat.jpg',
+    pic: '/images/catCafe.jpg',
     founded: 2020
 }])
+.then(() => {
+    console.log('It do be working')
+    process.exit()
+})
+.catch(err => {
+    console.log('You fail.', err)
+    process.exit()
+})
