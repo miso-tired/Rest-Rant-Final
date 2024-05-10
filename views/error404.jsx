829029953ffe3d959be942d2ce28cf2e94f2aaf9
Anthony <anthonylsa12@gmail.com>
2024-05-09 19:18:21 -0700

    fixed text and image alignment on error404 page

--- views/error404.jsx
+++ views/error404.jsx
@@ -1,21 +1,25 @@
-const React = require('react')
-const Def = require('./default.jsx')
+const React = require('react');
+const Def = require('./default.jsx');
 
-function error404 () {
+function error404() {
     return (
         <Def>
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oopsies!</p>
-                <div>
-                    <img src='/images/chiaSeeds.jpg' alt='Chia Seeds' className='centeredImage'/>
-                <div className='author-creds'>
-                    Photo by <a href='https://unsplash.com/@cravethebenefits'>Brenda Godinez</a> on <a href='https://unsplash.com/photos/two-fruit-beverages-on-glass-cups-MsTOg6rhRVk'></a>
-                </div>
+                <div className='centeredImage'>
+                    <div>
+                        <img src='/images/chiaSeeds.jpg' alt='Chia Seeds' />
+                        <div className='author-creds'>
+                            <div>
+                                Photo by <a href='https://unsplash.com/@cravethebenefits'>Brenda Godinez</a> on <a href='https://unsplash.com/photos/two-fruit-beverages-on-glass-cups-MsTOg6rhRVk'>Unsplash</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </main>
         </Def>
     )
 }
 
-module.exports = error404
+module.exports = error404;
--- views/home.jsx
+++ views/home.jsx
@@ -5,15 +5,23 @@ function home () {
     return (
         <Def>
             <main>
-                <h1>Home</h1>
+                <h1>Rest-Rant</h1>
                 <div>
-                <img src="/images/restaurantFront.jpg" alt='Restaurant Front'  className='centeredImage'/>
+                    <div className='centeredImage'>
+                        <img src='/images/restaurantFront.jpg' alt='Restaurant Front'/>
+                        <img src='/images/dayRestaurant.jpg' alt='Restaurant Front' />
+                    </div>
                 <div className='author-creds'>
-                    Photo by <a href='https://unsplash.com/@mroz'>Filip Mroz</a> on <a href='https://unsplash.com/photos/black-and-gray-food-stand-during-nighttime-oVMjj1ub5kw'></a>
+                <div>
+                    Photo by <a href='https://unsplash.com/@mroz'>Filip Mroz</a> on <a href='https://unsplash.com/photos/black-and-gray-food-stand-during-nighttime-oVMjj1ub5kw'>Unsplash</a>
+                    </div>
+                    <div>
+                    Photo by <a href='https://unsplash.com/@heftiba'>Toa Heftiba</a> on <a href='https://unsplash.com/photos/opened-white-wooden-door-50ijCEHhN8o'>Unsplash</a>
+                    </div>
                 </div>
                 </div>
                 <a href="/places">
-                    <button className="btn-primary">Places Page</button>
+                    <button className="btn btn-primary">Places Page</button>
                 </a>
             </main>
         </Def>
